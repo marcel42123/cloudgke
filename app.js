1f6bf7a3583dd4ac1ef54a32e9262cf1af5ef358
marcel42123 <129820006+marcel42123@users.noreply.github.com>
2023-04-04 13:11:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('marcelino ponto \n 52020008 \n Teknik informatika')
 })
 
 app.listen(PORT, HOST)
